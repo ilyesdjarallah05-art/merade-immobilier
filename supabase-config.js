b5360fd30afef002bdc2049f9beeaa4e25e9ac38
Ilyes Djar <ilyes.djarallah05@gmail.com>
2026-07-20 17:50:06 +0100

    Describe the changes

--- supabase-config.js
+++ supabase-config.js
@@ -3,6 +3,6 @@
 // Use the project BASE URL only, not /rest/v1.
 window.MERADE_SUPABASE = {
   url: 'https://hfgtwwdfpwrrzyqlxwaa.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmZ3R3d2RmcHdycnp5cWx4d2FhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMjQ3MjMsImV4cCI6MjA5NzkwMDcyM30.jG5Z0AjanVcFn1pcOIDF4rmjJndi7l5OfMdhZLiTznI',
+  anonKey: 'sb_publishable_b0QHfq2RJQahEBrd7NU4AQ_xf0f6W2l',
   storageBucket: 'property-media'
 };
